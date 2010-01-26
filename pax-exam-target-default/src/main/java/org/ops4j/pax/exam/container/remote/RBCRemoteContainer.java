20c912d26cd53c12805262b87c5a57fc6dee6606
Toni Menzel <toni@okidokiteam.com>
2010-01-26 09:15:10 +0100

    Now remote target impl is also a test container

---
+++ pax-exam-target-default/src/main/java/org/ops4j/pax/exam/container/remote/RBCRemoteContainer.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2009 Toni Menzel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.exam.container.remote;
+
+import java.io.InputStream;
+import org.ops4j.pax.exam.spi.container.TestContainer;
+import org.ops4j.pax.exam.spi.container.TestContainerException;
+import org.ops4j.pax.exam.spi.container.TestTarget;
+import org.ops4j.pax.exam.spi.container.TimeoutException;
+
+/**
+ * Graceful delegator to underlying target instance.
+ * All other TestContainer services are do nothing calls.
+ *
+ * @author Toni Menzel
+ * @since Jan 26, 2010
+ */
+public class RBCRemoteContainer implements TestContainer
+{
+
+    final private TestTarget m_target;
+
+    public RBCRemoteContainer( TestTarget target )
+    {
+        m_target = target;
+    }
+
+    public <T> T getService( Class<T> serviceType, String filter, long timeoutInMillis )
+        throws TestContainerException
+    {
+        return m_target.getService( serviceType, filter, timeoutInMillis );
+    }
+
+    public long installBundle( InputStream stream )
+    {
+        return m_target.installBundle( stream );
+    }
+
+    public void uninstallBundle( long id )
+    {
+        m_target.uninstallBundle( id );
+    }
+
+    public void setBundleStartLevel( long bundleId, int startLevel )
+        throws TestContainerException
+    {
+        // do nothing
+    }
+
+    public TestContainer start()
+        throws TimeoutException
+    {
+        // do nothing
+        return this;
+    }
+
+    public TestContainer stop()
+        throws TimeoutException
+    {
+        // do nothing
+        return this;
+    }
+
+    public void waitForState( long bundleId, int state, long timeoutInMillis )
+        throws TimeoutException
+    {
+        // do nothing
+    }
+
+
+}
