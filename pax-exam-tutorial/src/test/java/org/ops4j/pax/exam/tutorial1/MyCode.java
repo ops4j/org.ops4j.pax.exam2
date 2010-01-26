30b9690cae7031f3bbb2b4fa832c17d2c4db8abd
Toni Menzel <toni@okidokiteam.com>
2010-01-26 13:34:03 +0100

    move demo to tutorials project

--- pax-exam-tutorial/src/test/java/org/ops4j/pax/exam/tutorial1/MyCode.java
+++ pax-exam-tutorial/src/test/java/org/ops4j/pax/exam/tutorial1/MyCode.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.ops4j.pax.exam.raw;
+package org.ops4j.pax.exam.tutorial1;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
