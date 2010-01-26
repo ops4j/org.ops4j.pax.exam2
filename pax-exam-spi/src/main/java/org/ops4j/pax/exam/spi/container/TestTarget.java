48714056fd81fc35079fce6821c3061e843673a1
Toni Menzel <toni@okidokiteam.com>
2010-01-26 07:59:10 +0100

    fixed license headers

--- pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/container/TestTarget.java
+++ pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/container/TestTarget.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009 Toni Menzel.
+ * Copyright 2009,2010 Toni Menzel.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
