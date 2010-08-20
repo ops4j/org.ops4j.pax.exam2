90befdba31ab516dffc8982dda3540e0224b6ffb
tonit <toni@okidokiteam.com>
2010-08-20 18:08:27 +0200

    Make service lookup safe.

--- core/pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/container/DefaultRaw.java
+++ core/pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/container/DefaultRaw.java
@@ -25,6 +25,7 @@ import java.lang.reflect.Modifier;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.ops4j.pax.exam.TestTarget;
@@ -37,37 +38,31 @@ import org.ops4j.pax.exam.spi.probesupport.TestProbeBuilderImpl;
 /**
  * @author Toni Menzel
  * @since Jan 11, 2010
- *
+ *        <p/>
  *        TODO: to be changed into service
- *
  */
-public class DefaultRaw
-{
+public class DefaultRaw {
 
-    private static Logger LOG = LoggerFactory.getLogger( DefaultRaw.class );
+    private static Logger LOG = LoggerFactory.getLogger(DefaultRaw.class);
 
     private static int id = 0;
     private static final String PAX_EXAM_EXECUTABLE_SIG = "PaxExam-Executable-SIG";
     private static final String PROBE_SIGNATURE_KEY = "Probe-Signature";
 
-    public static TestProbeBuilder createProbe()
-    {
+    public static TestProbeBuilder createProbe() {
         return new TestProbeBuilderImpl();
     }
 
-    public static TestAddress call( Class clazz, String method )
-    {
-        return new ClassMethodTestAddress( PAX_EXAM_EXECUTABLE_SIG + ( id++ ), clazz, method );
+    public static TestAddress call(Class clazz, String method) {
+        return new ClassMethodTestAddress(PAX_EXAM_EXECUTABLE_SIG + (id++), clazz, method);
     }
 
-    public static TestAddress[] call( Class clazz )
-    {
+    public static TestAddress[] call(Class clazz) {
         List<TestAddress> calls = new ArrayList<TestAddress>();
-        for( String m : parseMethods( clazz ) )
-        {
-            calls.add( new ClassMethodTestAddress( PAX_EXAM_EXECUTABLE_SIG + ( id++ ), clazz, m ) );
+        for (String m : parseMethods(clazz)) {
+            calls.add(new ClassMethodTestAddress(PAX_EXAM_EXECUTABLE_SIG + (id++), clazz, m));
         }
-        return calls.toArray( new TestAddress[calls.size()] );
+        return calls.toArray(new TestAddress[calls.size()]);
     }
 
     /**
@@ -75,72 +70,63 @@ public class DefaultRaw
      *
      * @param clazz
      */
-    private static String[] parseMethods( Class clazz )
-    {
+    private static String[] parseMethods(Class clazz) {
         List<String> calls = new ArrayList<String>();
 
-        for( Method m : clazz.getDeclaredMethods() )
-        {
-            if( Modifier.isPublic( m.getModifiers() ) )
-            {
-                calls.add( m.getName() );
-            }
-            else
-            {
-                LOG.debug( "Skipping " + clazz.getName() + " Method " + m.getName() + " (not public)" );
+        for (Method m : clazz.getDeclaredMethods()) {
+            if (Modifier.isPublic(m.getModifiers())) {
+                calls.add(m.getName());
+            } else {
+                LOG.debug("Skipping " + clazz.getName() + " Method " + m.getName() + " (not public)");
             }
         }
-        return calls.toArray( new String[calls.size()] );
+        return calls.toArray(new String[calls.size()]);
     }
 
-    public static void execute( TestTarget target, TestAddress call )
-        throws InvocationTargetException, ClassNotFoundException, IllegalAccessException, InstantiationException
-    {
+    public static void execute(TestTarget target, TestAddress address)
+            throws InvocationTargetException, ClassNotFoundException, IllegalAccessException, InstantiationException {
 
-        assert ( target != null ) : "TestTarget must not be null.";
-        target.getService( ProbeInvoker.class, "(" + PROBE_SIGNATURE_KEY + "=" + call.signature() + ")", 0 ).call();
-    }
+        assert (target != null) : "TestTarget must not be null.";
+        assert (address != null) : "TestAddress must not be null.";
 
-    public static InputStream fromURL( String s )
-    {
-        try
-        {
-            return new URL( s ).openStream();
-        } catch( IOException e )
-        {
-            throw new RuntimeException( e );
+        String filterExpression = "(" + PROBE_SIGNATURE_KEY + "=" + address.signature() + ")";
+        ProbeInvoker service = target.getService(ProbeInvoker.class, filterExpression, 0);
+        if (service != null) {
+            service.call();
+        } else {
+            throw new IllegalStateException( "ProbeInvoker with service filter: " + filterExpression + " not available."  );
         }
     }
 
-    public static TestProbeProvider probe( final InputStream is, final String... testsSignatures )
-    {
-        return new TestProbeProvider()
-        {
+    public static InputStream fromURL(String s) {
+        try {
+            return new URL(s).openStream();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-            public TestAddress[] getTests()
-            {
+    public static TestProbeProvider probe(final InputStream is, final String... testsSignatures) {
+        return new TestProbeProvider() {
+
+            public TestAddress[] getTests() {
                 List<TestAddress> calls = new ArrayList<TestAddress>();
-                for( final String test : testsSignatures )
-                {
-                    calls.add( new TestAddress()
-                    {
-                        public String getInstruction()
-                        {
+                for (final String test : testsSignatures) {
+                    calls.add(new TestAddress() {
+                        public String getInstruction() {
                             return null;
                         }
 
-                        public String signature()
-                        {
+                        public String signature() {
                             return test;
                         }
                     }
                     );
                 }
-                return calls.toArray( new TestAddress[calls.size()] );
+                return calls.toArray(new TestAddress[calls.size()]);
             }
 
-            public InputStream getStream()
-            {
+            public InputStream getStream() {
                 return is;
             }
         };
