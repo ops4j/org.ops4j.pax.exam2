abdf916e32fbf1e927c12926d302265663678a4a
tonit <toni@okidokiteam.com>
2010-09-07 19:06:18 +0200

    document the reactor idea.

--- core/pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/ExxamReactor.java
+++ core/pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/ExxamReactor.java
@@ -1,18 +1,37 @@
 package org.ops4j.pax.exam.spi;
 
 import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.spi.StagedExamReactor;
-import org.ops4j.pax.exam.spi.TestProbeBuilder;
 
 /**
- * TODO doc.
+ * Part of the Plumbing Level API.
+ * The container creation control process needs a higher level control.
+ * Instead of directly creating containers out of {@link org.ops4j.pax.exam.TestContainerFactory} it is recommended
+ * to use {@link ExxamReactor} instead.
+ * <p/>
+ * You basically add some {@link Option}s and probes ({@link TestProbeBuilder}
+ * and get a retrieve a {@link org.ops4j.pax.exam.spi.StagedExamReactor} that can be used to retrieve containers.
+ * Now its up to the reactor and its configuration when to use and re-use the TestContainers.
  */
-public interface ExxamReactor
-{
+public interface ExxamReactor {
 
-    void addConfiguration( Option[] options );
+    /**
+     * Add the Configuration that contribute to desired container(s) you will get after calling {@link #stage()}.
+     *
+     * @param options a set of (user-end) options.
+     */
+    void addConfiguration(Option[] options);
 
-    void addProbe( TestProbeBuilder addTest );
+    /**
+     * Add the probe that contribute to desired container(s) you will get after calling {@link #stage()}.
+     *
+     * @param addTest builder that will (in the end) create a probe to be installed in the target container.
+     */
+    void addProbe(TestProbeBuilder addTest);
 
+    /**
+     * Last thing you call on the reactor usually. This gives you a unmodifiable Reactor version.
+     *
+     * @return new reactor that can be used to derive TestContainer instances.
+     */
     StagedExamReactor stage();
 }
--- core/pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/StagedExamReactor.java
+++ core/pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/StagedExamReactor.java
@@ -2,13 +2,24 @@ package org.ops4j.pax.exam.spi;
 
 /**
  * Separates logical test invocations from underlying reactor strategy.
+ * You get an instance from {@link org.ops4j.pax.exam.spi.ExxamReactor}
  */
-public interface StagedExamReactor
-{
+public interface StagedExamReactor {
 
-    void invoke( TestAddress call )
-        throws Exception;
+    /**
+     * Invoke an actual test. The reactor implementation will take care of (perhaps) instantiating a TestContainer or
+     * reusing an existing one and passing the call.
+     * You get the {@link TestAddress} from {@link org.ops4j.pax.exam.spi.TestProbeBuilder#getTests()}.
+     *
+     * @param address reference to a concrete, single test.
+     * @throws Exception in case of a problem.
+     */
+    void invoke(TestAddress address) throws Exception;
 
+    /**
+     * When you are done with using your reactor make sure to call this method so underlying resources (like TestContainers
+     * and connections) can be cleaned up.
+     */
     void tearDown();
 
 }
