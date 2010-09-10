4abd5f121402823afd50ad24ea084f14c0702c8d
tonit <toni@okidokiteam.com>
2010-09-10 14:13:32 +0200

    removed redundancy. added minimal doc

--- core/pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/container/CompositeOptionDescription.java
+++ core/pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/container/CompositeOptionDescription.java
@@ -15,50 +15,39 @@
  */
 package org.ops4j.pax.exam.spi.container;
 
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.OptionDescription;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.OptionDescription;
-import org.ops4j.pax.exam.TestContainer;
 
 /**
- * Created by IntelliJ IDEA.
- * User: tonit
- * Date: Jul 5, 2010
- * Time: 9:37:31 AM
- * To change this template use File | Settings | File Templates.
+ * Composite implementation of {@link OptionDescription}, so many Option Description appear as a single one.
  */
-public class CompositeOptionDescription implements OptionDescription
-{
+public class CompositeOptionDescription implements OptionDescription {
 
-    private List<OptionDescription> m_list;
     private Option[] m_used;
     private Option[] m_unused;
 
-    public CompositeOptionDescription( List<OptionDescription> descriptions )
-    {
-        m_list = descriptions;
+    public CompositeOptionDescription(List<OptionDescription> descriptions) {
         final List<Option> used = new ArrayList<Option>();
         final List<Option> unused = new ArrayList<Option>();
 
-        for( OptionDescription option : m_list )
-        {
-            used.addAll( Arrays.asList( option.getUsedOptions() ) );
-            unused.addAll( Arrays.asList( option.getIgnoredOptions() ) );
+        for (OptionDescription option : descriptions) {
+            used.addAll(Arrays.asList(option.getUsedOptions()));
+            unused.addAll(Arrays.asList(option.getIgnoredOptions()));
         }
-        m_used = used.toArray( new Option[used.size()] );
-        m_unused = unused.toArray( new Option[unused.size()] );
+        m_used = used.toArray(new Option[used.size()]);
+        m_unused = unused.toArray(new Option[unused.size()]);
 
     }
 
-    public Option[] getUsedOptions()
-    {
+    public Option[] getUsedOptions() {
         return m_used;
     }
 
-    public Option[] getIgnoredOptions()
-    {
+    public Option[] getIgnoredOptions() {
         return m_unused;
     }
 
