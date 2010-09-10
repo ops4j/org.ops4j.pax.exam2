122b4400d05ac64ecc5eb0d147155ec1898b1902
tonit <toni@okidokiteam.com>
2010-09-10 14:13:57 +0200

    error handling fixed (was totally silent before)

--- containers/pax-exam-container-native/src/main/java/org/ops4j/pax/exam/nat/internal/NativeTestContainer.java
+++ containers/pax-exam-container-native/src/main/java/org/ops4j/pax/exam/nat/internal/NativeTestContainer.java
@@ -40,224 +40,183 @@ import java.util.*;
  * @author Toni Menzel
  * @since Jan 7, 2010
  */
-public class NativeTestContainer implements TestContainer
-{
+public class NativeTestContainer implements TestContainer {
 
-    private static Logger LOG = LoggerFactory.getLogger( NativeTestContainer.class );
+    private static Logger LOG = LoggerFactory.getLogger(NativeTestContainer.class);
     final private List<String> m_bundles;
 
     private Framework m_framework;
     private Stack<Long> m_installed;
 
-    public NativeTestContainer( ArrayList<String> bundles )
-    {
+    public NativeTestContainer(ArrayList<String> bundles) {
         m_bundles = bundles;
     }
 
-    public <T> T getService( Class<T> serviceType, String filter, long timeout )
-        throws TestContainerException
-    {
+    public <T> T getService(Class<T> serviceType, String filter, long timeout)
+            throws TestContainerException {
         T service = null;
         long start = System.currentTimeMillis();
 
-        LOG.debug( "Aquiring Service " + serviceType.getName() + " " + ( filter != null ? filter : "" ) );
+        LOG.debug("Aquiring Service " + serviceType.getName() + " " + (filter != null ? filter : ""));
 
-        do
-        {
-            try
-            {
-                ServiceReference[] reference = m_framework.getBundleContext().getServiceReferences( serviceType.getName(), filter );
-                if( reference != null )
-                {
+        do {
+            try {
+                ServiceReference[] reference = m_framework.getBundleContext().getServiceReferences(serviceType.getName(), filter);
+                if (reference != null) {
 
-                    for( ServiceReference ref : reference )
-                    {
-                        return ( (T) m_framework.getBundleContext().getService( ref ) );
+                    for (ServiceReference ref : reference) {
+                        return ((T) m_framework.getBundleContext().getService(ref));
                     }
                 }
 
-            } catch( Exception e )
-            {
-                LOG.error( "Some problem during looking up service. " + e );
+            } catch (Exception e) {
+                LOG.error("Some problem during looking up service. " + e);
             }
-        } while( ( System.currentTimeMillis() ) < start + timeout );
+        } while ((System.currentTimeMillis()) < start + timeout);
 
-        LOG.debug( "Not found a matching Service " + serviceType.getName() + " for Filter:" + ( filter != null ? filter : "" ) );
+        LOG.debug("Not found a matching Service " + serviceType.getName() + " for Filter:" + (filter != null ? filter : ""));
 
         return null;
     }
 
-    public long install( InputStream stream )
-    {
+    public long install(InputStream stream) {
 
-        try
-        {
-            if( m_installed == null )
-            {
+        try {
+            if (m_installed == null) {
                 m_installed = new Stack<Long>();
             }
-            Bundle b = m_framework.getBundleContext().installBundle( "local", stream );
-            m_installed.push( b.getBundleId() );
-            LOG.debug( "Installed bundle " + b.getSymbolicName() + " as Bundle ID " + b.getBundleId() );
+            Bundle b = m_framework.getBundleContext().installBundle("local", stream);
+            m_installed.push(b.getBundleId());
+            LOG.debug("Installed bundle " + b.getSymbolicName() + " as Bundle ID " + b.getBundleId());
 
             // stream.close();
             b.start();
             return b.getBundleId();
-        } catch( BundleException e )
-        {
+        } catch (BundleException e) {
             e.printStackTrace();
         }
         return -1;
     }
 
-    public void cleanup()
-    {
-        if( m_installed != null )
-        {
-            while( ( !m_installed.isEmpty() ) )
-            {
-                try
-                {
+    public void cleanup() {
+        if (m_installed != null) {
+            while ((!m_installed.isEmpty())) {
+                try {
                     Long id = m_installed.pop();
-                    Bundle bundle = m_framework.getBundleContext().getBundle( id );
+                    Bundle bundle = m_framework.getBundleContext().getBundle(id);
                     bundle.uninstall();
-                    LOG.debug( "Uninstalled bundle " + id );
-                } catch( BundleException e )
-                {
+                    LOG.debug("Uninstalled bundle " + id);
+                } catch (BundleException e) {
                     e.printStackTrace();
                 }
             }
         }
     }
 
-    public void setBundleStartLevel( long bundleId, int startLevel )
-        throws TestContainerException
-    {
-        try
-        {
-            m_framework.getBundleContext().getBundle( bundleId ).start( startLevel );
-        } catch( BundleException e )
-        {
+    public void setBundleStartLevel(long bundleId, int startLevel)
+            throws TestContainerException {
+        try {
+            m_framework.getBundleContext().getBundle(bundleId).start(startLevel);
+        } catch (BundleException e) {
             e.printStackTrace();
         }
     }
 
     public TestContainer stop()
-        throws TimeoutException
-    {
-        if( m_framework != null )
-        {
+            throws TimeoutException {
+        if (m_framework != null) {
 
-            try
-            {
-                LOG.debug( "Framework goes down.." );
+            try {
+                LOG.debug("Framework goes down..");
                 m_framework.stop();
-                m_framework.waitForStop( 1000 );
+                m_framework.waitForStop(1000);
 
 
-            } catch( BundleException e )
-            {
-                e.printStackTrace();
-            } catch( InterruptedException e )
-            {
-
+            } catch (BundleException e) {
+                 LOG.warn("Problem during stopping fw.", e);
+            } catch (InterruptedException e) {
+                LOG.warn("InterruptedException during stopping fw.", e);
             }
-        }
-        else
-        {
-            throw new IllegalStateException( "Framework does not exist. Called start() before ? " );
+        } else {
+            throw new IllegalStateException("Framework does not exist. Called start() before ? ");
         }
         return this;
     }
 
-    public void waitForState( long bundleId, int state, long timeoutInMillis )
-        throws TimeoutException
-    {
+    public void waitForState(long bundleId, int state, long timeoutInMillis)
+            throws TimeoutException {
         // look for a certain state in fw
     }
 
     public TestContainer start()
-        throws TimeoutException
-    {
+            throws TimeoutException {
         ClassLoader parent = null;
-        try
-        {
+        try {
             final Map<String, String> p = new HashMap<String, String>();
-            String folder = System.getProperty( "user.home" ) + File.separator + "osgi";
-            LOG.debug( "Cache folder set to " + folder );
-            FileUtils.delete( new File( folder ) );
+            String folder = System.getProperty("user.home") + File.separator + "osgi";
+            LOG.debug("Cache folder set to " + folder);
+            FileUtils.delete(new File(folder));
             // load default stuff
 
-            p.put( "org.osgi.framework.storage", folder );
+            p.put("org.osgi.framework.storage", folder);
             //  System.setProperty( "org.osgi.vendor.framework", "org.ops4j.pax.exam" );
 
-            p.put( "org.osgi.framework.system.packages.extra", "org.ops4j.pax.exam.raw.extender;version=" + skipSnapshotFlag( Info.getPaxExamVersion() ) );
+            p.put("org.osgi.framework.system.packages.extra", "org.ops4j.pax.exam.raw.extender;version=" + skipSnapshotFlag(Info.getPaxExamVersion()));
             // TODO fix ContextClassLoaderUtils.doWithClassLoader() and replace logic with it.
             parent = Thread.currentThread().getContextClassLoader();
-            Thread.currentThread().setContextClassLoader( null );
+            Thread.currentThread().setContextClassLoader(null);
 
-            FrameworkFactory factory = (FrameworkFactory) DiscoverSingleton.find( FrameworkFactory.class );
+            FrameworkFactory factory = (FrameworkFactory) DiscoverSingleton.find(FrameworkFactory.class);
 
-            m_framework = factory.newFramework( p );
+            m_framework = factory.newFramework(p);
 
             m_framework.init();
 
             BundleContext context = m_framework.getBundleContext();
-            for( String bundle : m_bundles )
-            {
-                Bundle b = context.installBundle( bundle );
-                LOG.debug( "Installed bundle " + b.getSymbolicName() + " as Bundle ID " + b.getBundleId() );
+            for (String bundle : m_bundles) {
+                Bundle b = context.installBundle(bundle);
+                LOG.debug("Installed bundle " + b.getSymbolicName() + " as Bundle ID " + b.getBundleId());
 
             }
             m_framework.start();
-            for( Bundle b : m_framework.getBundleContext().getBundles() )
-            {
+            for (Bundle b : m_framework.getBundleContext().getBundles()) {
                 b.start();
-                LOG.debug( "Started: " + b.getSymbolicName() );
+                LOG.debug("Started: " + b.getSymbolicName());
             }
-            Thread.currentThread().setContextClassLoader( parent );
+            Thread.currentThread().setContextClassLoader(parent);
 
-        } catch( Exception e )
-        {
+        } catch (Exception e) {
             e.printStackTrace();
-        } finally
-        {
-            if( parent != null )
-            {
-                Thread.currentThread().setContextClassLoader( parent );
+        } finally {
+            if (parent != null) {
+                Thread.currentThread().setContextClassLoader(parent);
 
             }
         }
         return this;
     }
 
-    private String skipSnapshotFlag( String version )
-    {
-        int idx = version.indexOf( "-" );
-        if( idx >= 0 )
-        {
-            return version.substring( 0, idx );
-        }
-        else
-        {
+    private String skipSnapshotFlag(String version) {
+        int idx = version.indexOf("-");
+        if (idx >= 0) {
+            return version.substring(0, idx);
+        } else {
             return version;
         }
     }
 
-    private FrameworkFactory getFrameworkFactory( String factoryClass )
-        throws ClassNotFoundException
+    private FrameworkFactory getFrameworkFactory(String factoryClass)
+            throws ClassNotFoundException
 
     {
         return null;//return (FrameworkFactory) Class.forName( factoryClass );
     }
 
-    private String getFelixFactory()
-    {
+    private String getFelixFactory() {
         return "org.apache.felix.framework.FrameworkFactory";
     }
 
-    private String getEquinoxFactory()
-    {
+    private String getEquinoxFactory() {
         return "org.eclipse.osgi.launch.EquinoxFactory";
     }
 
