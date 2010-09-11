5cf9c502098378330a0daa3b13332d37dfdc8cc8
tonit <toni@okidokiteam.com>
2010-09-11 13:25:09 +0200

    added test for parser impl

---
+++ containers/pax-exam-container-remote/src/test/java/org/ops4j/pax/exam/container/remote/ParserTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2010 Okidokiteam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.exam.container.remote;
+
+import org.junit.Test;
+import org.ops4j.pax.exam.CoreOptions;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.container.remote.options.RBCPortOption;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test for this modules Options Parser implementation.
+ */
+public class ParserTest {
+
+    @Test
+    public void testDefaults() {
+        Option[] options = CoreOptions.options();
+
+        Parser parser = new Parser(options);
+
+        assertThat(parser.getHost(), is(RBCPortOption.DEFAULTHOST));
+        assertThat(parser.getRMIPort(), is(RBCPortOption.DEFAULTPORT));
+    }
+
+    @Test
+    public void testParsing() {
+        Option[] options = CoreOptions.options(
+                RBCRemoteTargetOptions.location("bee", 1234)
+        );
+
+        Parser parser = new Parser(options);
+
+        assertThat(parser.getHost(), is("bee"));
+        assertThat(parser.getRMIPort(), is(1234));
+
+    }
+}
