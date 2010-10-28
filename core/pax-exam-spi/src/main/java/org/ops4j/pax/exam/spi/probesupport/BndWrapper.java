d8c102ed0541066c49d4065e86dbfdf124387102
tonit <toni@okidokiteam.com>
2010-10-28 10:44:48 +0200

    include direct use of bnd api (more customization needed)

---
+++ core/pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/probesupport/BndWrapper.java
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2010 Toni Menzel.
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.exam.spi.probesupport;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.Properties;
+import java.util.jar.Manifest;
+import aQute.lib.osgi.Analyzer;
+import aQute.lib.osgi.Jar;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ops4j.lang.NullArgumentException;
+
+/**
+ *
+ */
+public class BndWrapper
+{
+
+    /**
+     * Logger.
+     */
+    private static final Log LOG = LogFactory.getLog( BndWrapper.class );
+
+    /**
+     * Utility class. Ment to be used using static methods
+     */
+    private BndWrapper()
+    {
+        // utility class
+    }
+
+    /**
+     * Processes the input jar and generates the necessary OSGi headers using specified instructions.
+     *
+     * @param jarInputStream input stream for the jar to be processed. Cannot be null.
+     * @param instructions   bnd specific processing instructions. Cannot be null.
+     * @param jarInfo        information about the jar to be processed. Usually the jar url. Cannot be null or empty.
+     *
+     * @return an input stream for the generated bundle
+     *
+     * @throws NullArgumentException if any of the parameters is null
+     * @throws IOException           re-thron during jar processing
+     */
+    public static InputStream createBundle( final InputStream jarInputStream,
+
+                                            final String jarInfo,
+
+                                            final Properties... instructions )
+        throws IOException
+    {
+        NullArgumentException.validateNotNull( jarInputStream, "Jar URL" );
+        NullArgumentException.validateNotNull( instructions, "Instructions" );
+        NullArgumentException.validateNotEmpty( jarInfo, "Jar info" );
+
+        LOG.debug( "Creating bundle for [" + jarInfo + "]" );
+
+        final Jar jar = new Jar( "dot", jarInputStream );
+        final Manifest manifest = jar.getManifest();
+
+        // Make the jar a bundle if it is not already a bundle
+
+        final Analyzer analyzer = new Analyzer();
+        analyzer.setJar( jar );
+        for( Properties in : instructions )
+        {
+            LOG.trace( "+ Using instructions " + in );
+            // Do not use instructions as default for properties because it looks like BND uses the props
+            // via some other means then getProperty() and so the instructions will not be used at all
+            // So, just copy instructions to properties
+            analyzer.setProperties( in );
+            analyzer.mergeManifest( manifest );
+        }
+
+        checkMandatoryProperties( analyzer, jar, jarInfo );
+        analyzer.calcManifest();
+
+        return createInputStream( jar );
+    }
+
+    /**
+     * Creates an piped input stream for the wrapped jar.
+     * This is done in a thread so we can return quickly.
+     *
+     * @param jar the wrapped jar
+     *
+     * @return an input stream for the wrapped jar
+     *
+     * @throws java.io.IOException re-thrown
+     */
+    private static PipedInputStream createInputStream( final Jar jar )
+        throws IOException
+    {
+        final PipedInputStream pin = new PipedInputStream();
+        final PipedOutputStream pout = new PipedOutputStream( pin );
+
+        new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    jar.write( pout );
+                } catch( IOException e )
+                {
+                    LOG.warn( "Bundle cannot be generated" );
+                } finally
+                {
+                    try
+                    {
+                        jar.close();
+                        pout.close();
+                    } catch( IOException ignore )
+                    {
+                        // if we get here something is very wrong
+                        LOG.error( "Bundle cannot be generated", ignore );
+                    }
+                }
+            }
+        }.start();
+
+        return pin;
+    }
+
+    /**
+     * Check if manadatory properties are present, otherwise generate default.
+     *
+     * @param analyzer     bnd analyzer
+     * @param jar          bnd jar
+     * @param symbolicName bundle symbolic name
+     */
+    private static void checkMandatoryProperties( final Analyzer analyzer,
+                                                  final Jar jar,
+                                                  final String symbolicName )
+    {
+        final String importPackage = analyzer.getProperty( Analyzer.IMPORT_PACKAGE );
+        if( importPackage == null || importPackage.trim().length() == 0 )
+        {
+            analyzer.setProperty( Analyzer.IMPORT_PACKAGE, "*;resolution:=optional" );
+        }
+        final String exportPackage = analyzer.getProperty( Analyzer.EXPORT_PACKAGE );
+        if( exportPackage == null || exportPackage.trim().length() == 0 )
+        {
+            analyzer.setProperty( Analyzer.EXPORT_PACKAGE, analyzer.calculateExportsFromContents( jar ) );
+        }
+        final String localSymbolicName = analyzer.getProperty( Analyzer.BUNDLE_SYMBOLICNAME, symbolicName );
+        analyzer.setProperty( Analyzer.BUNDLE_SYMBOLICNAME, generateSymbolicName( localSymbolicName ) );
+    }
+
+    /**
+     * Processes symbolic name and replaces osgi spec invalid characters with "_".
+     *
+     * @param symbolicName bundle symbolic name
+     *
+     * @return a valid symbolic name
+     */
+    private static String generateSymbolicName( final String symbolicName )
+    {
+        return symbolicName.replaceAll( "[^a-zA-Z_0-9.-]", "_" );
+    }
+
+}
