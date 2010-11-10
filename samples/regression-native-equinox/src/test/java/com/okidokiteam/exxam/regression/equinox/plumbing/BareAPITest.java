896bfe4fe6277664272fe413c563aaf9823d8dfb
tonit <toni@okidokiteam.com>
2010-11-10 16:24:44 +0100

    added regression for equinox

---
+++ samples/regression-native-equinox/src/test/java/com/okidokiteam/exxam/regression/equinox/plumbing/BareAPITest.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2010 Okidokiteam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.okidokiteam.exxam.regression.equinox.plumbing;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.MethodRule;
+import org.junit.rules.TestWatchman;
+import org.junit.runners.model.FrameworkMethod;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.OptionDescription;
+import org.ops4j.pax.exam.TestAddress;
+import org.ops4j.pax.exam.TestContainer;
+import org.ops4j.pax.exam.TestContainerFactory;
+import org.ops4j.pax.exam.TestProbeProvider;
+import org.ops4j.pax.exam.spi.container.PaxExamRuntime;
+
+import static org.ops4j.pax.exam.LibraryOptions.*;
+import static org.ops4j.pax.exam.spi.container.DefaultRaw.*;
+
+/**
+ * Simple regression
+ */
+public class BareAPITest
+{
+
+    @Rule
+    public MethodRule watchman = new TestWatchman() {
+
+        @Override
+        public void finished( FrameworkMethod method )
+        {
+            System.out.println("Finished..." + method.getName());
+        }
+
+        public void starting(FrameworkMethod method) {
+
+            System.out.println("{} being run..." + method.getName());
+        }
+      };
+    
+
+    private TestContainerFactory getFactory()
+    {
+        return PaxExamRuntime.getTestContainerFactory();
+    }
+
+    /**
+     * Very low level.
+     *
+     * @throws Exception problems?
+     */
+    @Test
+    public void bareRunTest()
+        throws Exception
+    {
+        TestContainerFactory factory = getFactory();
+        Option[] options = new Option[]{ junitBundles(), easyMockBundles() };
+
+// the parse will split all single containers into dedicated OptionDescription(s)
+        for( OptionDescription testTarget : factory.parse( options ) )
+        {
+            TestContainer testContainer = factory.createContainer( testTarget );
+            try
+            {
+                testContainer.start();
+                TestProbeProvider probe = createProbe().addTest( Probe.class ).build();
+                testContainer.install( probe.getStream() );
+
+                for( TestAddress call : probe.getTests() )
+                {
+                    // this is a shortcut for getting the proper Service (ServiceInvoker currently) and calls the "invoke" with that call (handle)
+                    execute( testContainer, call );
+                }
+            } finally
+            {
+                testContainer.stop();
+            }
+        }
+    }
+
+    @Test
+    public void bareRunMoreThanOneProbeTest()
+        throws Exception
+    {
+        TestContainerFactory factory = getFactory();
+        Option[] options = new Option[]{ junitBundles(), easyMockBundles() };
+
+        // the parse will split all single containers into dedicated OptionDescription(s)
+        for( OptionDescription testTarget : factory.parse( options ) )
+        {
+            TestContainer testContainer = factory.createContainer( testTarget );
+            try
+            {
+                testContainer.start();
+                TestProbeProvider probe = createProbe().addTest( Probe.class ).addTest( Probe2.class ).build();
+
+                testContainer.install( probe.getStream() );
+
+                for( TestAddress call : probe.getTests() )
+                {
+                    // this is a shortcut for getting the proper Service (ServiceInvoker currently) and calls the "invoke" with that call (handle)
+                    execute( testContainer, call );
+                }
+            } finally
+            {
+                testContainer.stop();
+            }
+        }
+    }
+
+}
