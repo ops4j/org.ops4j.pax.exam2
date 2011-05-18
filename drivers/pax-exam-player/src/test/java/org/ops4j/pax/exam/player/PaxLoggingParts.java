9273a3cf71d962ae241f7b4a887083b9ccdfe3d0
Toni Menzel <toni@okidokiteam.com>
2011-05-18 13:41:30 +0200

    added license header

--- drivers/pax-exam-player/src/test/java/org/ops4j/pax/exam/player/PaxLoggingParts.java
+++ drivers/pax-exam-player/src/test/java/org/ops4j/pax/exam/player/PaxLoggingParts.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2011 Toni Menzel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ops4j.pax.exam.player;
 
 import org.ops4j.pax.exam.Option;
@@ -20,7 +37,6 @@ public class PaxLoggingParts implements CompositeOption {
     public Option[] getOptions()
     {
         return options(
-            mavenBundle().groupId( "org.ops4j.pax.logging" ).artifactId( "pax-logging-api" ).version( m_version ),
             mavenBundle().groupId( "org.ops4j.pax.logging" ).artifactId( "pax-logging-service" ).version( m_version )
         );
     }
