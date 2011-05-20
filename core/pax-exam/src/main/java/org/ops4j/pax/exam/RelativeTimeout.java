a2056ea9d68724d5132fe3999971f964e7520894
Toni Menzel <toni@okidokiteam.com>
2011-05-20 11:34:12 +0200

    PAXEXAM-230 RelativeTimeout class to drive timout settings in exam system.

---
+++ core/pax-exam/src/main/java/org/ops4j/pax/exam/RelativeTimeout.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2011 Toni Menzel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.exam;
+
+/**
+ * Timout type that is being used in exam to specify a single timeout value but control different timout points in the system with that value.
+ * In most cases, clients will just use the value directly as time in milliseconds.
+ * 
+ * However, this type also says that the value is just a (possibly user specified) "hint" and not mean the exaclt value.
+ * You can see it as a relative timeout value.
+ * 
+ * @author Toni Menzel (toni@okidokiteam.com)
+ *
+ */
+public class RelativeTimeout {
+	
+	public final static RelativeTimeout TIMEOUT_NOWAIT = new RelativeTimeout(0L);
+	public final static RelativeTimeout TIMEOUT_NOTIMEOUT = new RelativeTimeout(Long.MAX_VALUE);
+	public final static RelativeTimeout TIMEOUT_DEFAULT = new RelativeTimeout(1000L);
+	
+	final private long m_value;
+
+	public RelativeTimeout( final long time ) {
+		m_value = time;
+	}
+	
+	public long getValue() {
+		return m_value;
+	}
+	
+	public boolean isNoWait() {
+		return this.equals(TIMEOUT_NOWAIT);
+	}
+	
+	public boolean isNoTimeout() {
+		return this.equals(TIMEOUT_NOTIMEOUT);
+	}
+	
+	public boolean isDefault() {
+		return this.equals(TIMEOUT_DEFAULT);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof RelativeTimeout) {
+			return ((RelativeTimeout) obj).getValue() == getValue();
+		}else 
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return getClass().getName().hashCode() + new Long(m_value).hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return "[ RelativeTimeout value = " + m_value + " ]";
+	}
+	
+	
+
+}
---
+++ core/pax-exam/src/test/java/org/ops4j/pax/exam/RelativeTimeoutTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2011 Toni Menzel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.exam;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Toni Menzel (toni@okidokiteam.com)
+ *
+ */
+public class RelativeTimeoutTest {
+	
+	@Test
+	public void testDefaults() {
+		RelativeTimeout rel = RelativeTimeout.TIMEOUT_DEFAULT;
+		assertThat( rel.isDefault(), is( equalTo(Boolean.TRUE))) ;
+		assertThat( rel.isNoWait(), is( equalTo(Boolean.FALSE))) ;
+		assertThat( rel.isNoTimeout(), is( equalTo(Boolean.FALSE))) ;
+	}
+	
+	@Test
+	public void testNoWait() {
+		RelativeTimeout rel = RelativeTimeout.TIMEOUT_NOWAIT;
+		assertThat( rel.isDefault(), is( equalTo(Boolean.FALSE))) ;
+		assertThat( rel.isNoWait(), is( equalTo(Boolean.TRUE))) ;
+		assertThat( rel.isNoTimeout(), is( equalTo(Boolean.FALSE))) ;
+	}
+	
+	@Test
+	public void testNoTimeout() {
+		RelativeTimeout rel = RelativeTimeout.TIMEOUT_NOTIMEOUT;
+		assertThat( rel.isDefault(), is( equalTo(Boolean.FALSE))) ;
+		assertThat( rel.isNoWait(), is( equalTo(Boolean.FALSE))) ;
+		assertThat( rel.isNoTimeout(), is( equalTo(Boolean.TRUE))) ;
+	}
+	
+	@Test
+	public void testEquality() {
+		RelativeTimeout rel1 = new RelativeTimeout(1000);
+		RelativeTimeout rel2 = new RelativeTimeout(1000);
+		
+		assertThat( rel1, is( equalTo(rel2))) ;
+	}
+}
