aa894b9f3d939643a25f3d8b0d2ca5b4d19b1d1f
Toni Menzel <toni@okidokiteam.com>
2011-05-24 15:51:43 +0200

    PAXEXAM-229 do not copy over default options when "fork()"

--- core/pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/DefaultExamSystem.java
+++ core/pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/DefaultExamSystem.java
@@ -27,105 +27,134 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.io.Files;
 
-public class DefaultExamSystem implements ExamSystem {
+public class DefaultExamSystem implements ExamSystem
+{
 
-	private static final Logger LOG = LoggerFactory.getLogger(DefaultExamSystem.class);
+    private static final Logger LOG = LoggerFactory.getLogger( DefaultExamSystem.class );
 
-	final private Store<InputStream> m_store;
-	final private File m_configDirectory;
-	final private Option[] m_options;
+    final private Store<InputStream> m_store;
+    final private File m_configDirectory;
+    final private Option[] m_combinedOptions;
+    
+    final private Stack<File> m_tempStack;
+    final private Stack<ExamSystem> m_subsystems;
 
-	final private Stack<File> m_tempStack;
-	final private Stack<ExamSystem> m_subsystems;
+    final private RelativeTimeout m_timeout;
 
-	final private RelativeTimeout m_timeout;
+    /**
+     * Creates a fresh ExamSystem. Your options will be combined with internal defaults.
+     * If you need to change the system after it has been created, you fork() it.
+     * Forking will not add default options again.
+     * 
+     * @param options
+     * @return
+     * @throws IOException
+     */
+    public static ExamSystem create( Option[] options ) throws IOException
+    {
+        LOG.info( "Pax Exam System (Version: " + Info.getPaxExamVersion() + ") created." );
+        return new DefaultExamSystem( combine( options,localOptions() ) );
+    }
 
-	public static ExamSystem create(Option[] options) throws IOException {
-		LOG.info("!! Pax Exam System (Version: " + Info.getPaxExamVersion() + ") created.");
-		return new DefaultExamSystem(options);
-	}
+    /**
+     * Create a new system based on *this*.
+     * The forked System remembers the forked instances in order to clear resources up (if desired).
+     */
+    public ExamSystem fork( Option[] options ) throws IOException
+    {   
+        ExamSystem sys = new DefaultExamSystem( combine( m_combinedOptions, options ) );
+        m_subsystems.add( sys );
+        return sys;
+    }
 
-	public ExamSystem fork(Option[] options) throws IOException {
-		ExamSystem sys = new DefaultExamSystem(combine(m_options, options));
-		m_subsystems.add(sys);
-		return sys;
-	}
+    private DefaultExamSystem( Option[] options ) throws IOException
+    {
+        m_combinedOptions = expand(options);
+        m_tempStack = new Stack<File>();
+        m_subsystems = new Stack<ExamSystem>();
 
-	private DefaultExamSystem(Option[] options) throws IOException {
-		m_options = expand(combine(localOptions(), options));
-		m_tempStack = new Stack<File>();
-		m_subsystems = new Stack<ExamSystem>();
+        m_configDirectory = new File( System.getProperty( "user.home" ) + "/.pax/exam/" );
+        m_configDirectory.mkdirs();
+        m_store = new TemporaryStore( getTempFolder(), false );
+        m_timeout = RelativeTimeout.TIMEOUT_DEFAULT;
+    }
 
-		m_configDirectory = new File(System.getProperty("user.home") + "/.pax/exam/");
-		m_configDirectory.mkdirs();
-		m_store = new TemporaryStore(getTempFolder(), false);
-		m_timeout = RelativeTimeout.TIMEOUT_DEFAULT;
-	}
+    private static Option[] localOptions()
+    {
+        return new Option[] {
+                bootDelegationPackage( "sun.*" ),
+                url( "link:classpath:META-INF/links/org.ops4j.pax.exam.rbc.link" ).startLevel( START_LEVEL_SYSTEM_BUNDLES ),
+                url( "link:classpath:META-INF/links/org.ops4j.pax.extender.service.link" ).startLevel( START_LEVEL_SYSTEM_BUNDLES ),
+                url( "link:classpath:META-INF/links/org.osgi.compendium.link" ).startLevel( START_LEVEL_SYSTEM_BUNDLES ),
+                url( "link:classpath:META-INF/links/org.ops4j.pax.logging.api.link" ).startLevel( START_LEVEL_SYSTEM_BUNDLES ) };
+    }
 
-	private Option[] localOptions() {
-		return new Option[] {
-				bootDelegationPackage("sun.*"),
-				url("link:classpath:META-INF/links/org.ops4j.pax.exam.rbc.link" ).startLevel(START_LEVEL_SYSTEM_BUNDLES),
-				url("link:classpath:META-INF/links/org.ops4j.pax.extender.service.link" ).startLevel(START_LEVEL_SYSTEM_BUNDLES),
-				url("link:classpath:META-INF/links/org.osgi.compendium.link").startLevel(START_LEVEL_SYSTEM_BUNDLES),
-				url("link:classpath:META-INF/links/org.ops4j.pax.logging.api.link" ).startLevel ( START_LEVEL_SYSTEM_BUNDLES ) };		
-	}
+    public <T extends Option> T[] getOptions( final Class<T> optionType )
+    {
+        return filter( optionType, m_combinedOptions );
+    }
 
-	public <T extends Option> T[] getOptions(final Class<T> optionType) {
-		return filter(optionType, m_options);
-	}
+    /**
+     * 
+     * @return the basic directory that Exam should use to look at
+     *         user-defaults.
+     */
+    public File getConfigFolder()
+    {
+        return m_configDirectory;
+    }
 
-	/**
-	 * 
-	 * @return the basic directory that Exam should use to look at
-	 *         user-defaults.
-	 */
-	public File getConfigFolder() {
-		return m_configDirectory;
-	}
+    /**
+     * 
+     * @return the basic directory that Exam should use for all IO write
+     *         activities.
+     */
+    public File getTempFolder()
+    {
+        File tmp = Files.createTempDir();
+        m_tempStack.add( tmp );
+        return tmp;
+    }
 
-	/**
-	 * 
-	 * @return the basic directory that Exam should use for all IO write
-	 *         activities.
-	 */
-	public File getTempFolder() {
-		File tmp = Files.createTempDir();
-		m_tempStack.add(tmp);
-		return tmp;
-	}
+    /**
+     * 
+     * @return a relative indication of how to deal with timeouts.
+     */
+    public RelativeTimeout getTimeout()
+    {
+        return m_timeout;
+    }
 
-	/**
-	 * 
-	 * @return a relative indication of how to deal with timeouts.
-	 */
-	public RelativeTimeout getTimeout() {
-		return m_timeout;
-	}
+    /**
+     * 
+     * Clears up resources taken by system (like temporary files)
+     * 
+     */
+    public void clear()
+    {
+        try
+        {
+            for ( ExamSystem sys : m_subsystems )
+            {
+                sys.clear();
+            }
+            while ( !m_tempStack.isEmpty() )
+            {
+                Files.deleteRecursively( m_tempStack.pop().getCanonicalFile() );
+            }
+        } catch ( IOException e )
+        {
+            // LOG.info("Clearing " + this.toString() + " failed." ,e);
+        }
+    }
 
-	/**
-	 * 
-	 * Clears up resources taken by system (like temporary files)
-	 * 
-	 */
-	public void clear()  {
-		try {
-			for (ExamSystem sys : m_subsystems) {
-				sys.clear();
-			}
-			while (!m_tempStack.isEmpty()) {
-				Files.deleteRecursively(m_tempStack.pop().getCanonicalFile());
-			}
-		} catch(IOException e) {
-			//LOG.info("Clearing " + this.toString() + " failed." ,e);
-		}
-	}
+    public TestProbeBuilder createProbe( Properties p ) throws IOException
+    {
+        return new TestProbeBuilderImpl( p, m_store );
+    }
 
-	public TestProbeBuilder createProbe(Properties p) throws IOException {
-		return new TestProbeBuilderImpl(p, m_store);
-	}
-
-	public String createID(String purposeText) {
-		return UUID.randomUUID().toString();
-	}
+    public String createID( String purposeText )
+    {
+        return UUID.randomUUID().toString();
+    }
 }
