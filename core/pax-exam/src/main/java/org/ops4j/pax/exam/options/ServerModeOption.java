69f77b65a719eb55673b16e8e5dc74d5c8e895ac
Toni Menzel <toni@okidokiteam.com>
2011-05-30 12:20:37 +0200

    PAXEXAM-239 added option marker

---
+++ core/pax-exam/src/main/java/org/ops4j/pax/exam/options/ServerModeOption.java
@@ -0,0 +1,17 @@
+package org.ops4j.pax.exam.options;
+
+import org.ops4j.pax.exam.Option;
+import static org.ops4j.pax.exam.CoreOptions.*;
+
+
+/**
+ * Marker Option
+ * @author tonit
+ *
+ */
+public class ServerModeOption implements Option
+{
+
+   
+
+}
