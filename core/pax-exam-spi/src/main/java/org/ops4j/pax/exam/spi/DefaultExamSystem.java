22a58f4807a45240688b311ed157066f2447e925
Toni Menzel <toni@okidokiteam.com>
2011-07-22 12:18:09 +0200

    PAXEXAM-248 fixed. plus many more javadoc comments. Also made getSingleOption available in ExamSystem interface.

--- core/pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/DefaultExamSystem.java
+++ core/pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/DefaultExamSystem.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2010 Toni Menzel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ops4j.pax.exam.spi;
 
 import static org.ops4j.pax.exam.OptionUtils.combine;
@@ -30,117 +47,131 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.io.Files;
 
-public class DefaultExamSystem implements ExamSystem
-{
+/**
+ * {@literal DefaultExamSystem} represents the default implementation of {@link ExamSystem}.
+ *
+ * It takes care of options (including implicit defaults), temporary folders (and their cleanup) and
+ * cross cutting parameters that are frequently used like "timeout" values.
+ */
+public class DefaultExamSystem implements ExamSystem {
 
     private static final Logger LOG = LoggerFactory.getLogger( DefaultExamSystem.class );
 
     final private Store<InputStream> m_store;
     final private File m_configDirectory;
     final private Option[] m_combinedOptions;
-
     final private Stack<ExamSystem> m_subsystems;
-
     final private RelativeTimeout m_timeout;
-
-    private Set<Class> m_requestedOptionTypes = new HashSet<Class>();
-
-    private CleanCachesOption m_clean;
-
-    private File m_cache;
-
-    
+    final private Set<Class> m_requestedOptionTypes = new HashSet<Class>();
+    final private CleanCachesOption m_clean;
+    final private File m_cache;
 
     /**
      * Creates a fresh ExamSystem. Your options will be combined with internal defaults.
      * If you need to change the system after it has been created, you fork() it.
      * Forking will not add default options again.
-     * 
-     * @param options
-     * @return
-     * @throws IOException
+     *
+     * @param options options to be used to define the new system.
+     *
+     * @return a fresh instance of {@literal DefaultExamSystem}
+     *
+     * @throws IOException in case of an instantiation problem. (IO related)
      */
-    public static ExamSystem create( Option[] options ) throws IOException
+    public static ExamSystem create( Option[] options )
+        throws IOException
     {
         LOG.info( "Pax Exam System (Version: " + Info.getPaxExamVersion() + ") created." );
         return new DefaultExamSystem( options );
-       
+
     }
 
     /**
      * Create a new system based on *this*.
      * The forked System remembers the forked instances in order to clear resources up (if desired).
      */
-    public ExamSystem fork( Option[] options ) throws IOException
+    public ExamSystem fork( Option[] options )
+        throws IOException
     {
         ExamSystem sys = new DefaultExamSystem( combine( m_combinedOptions, options ) );
         m_subsystems.add( sys );
         return sys;
     }
 
-    private DefaultExamSystem( Option[] options ) throws IOException
+    /**
+     * Creates a fresh ExamSystem. Your options will be combined with internal defaults.
+     * If you need to change the system after it has been created, you fork() it.
+     * Forking will not add default options again.
+     *
+     * @param options options to be used to define the new system.
+     *
+     * @throws IOException in case of an instantiation problem. (IO related)
+     */
+    private DefaultExamSystem( Option[] options )
+        throws IOException
     {
         m_subsystems = new Stack<ExamSystem>();
         m_combinedOptions = expand( options );
         m_configDirectory = new File( System.getProperty( "user.home" ) + "/.pax/exam/" );
         m_configDirectory.mkdirs();
-        
-        WorkingDirectoryOption work = getSingleOption(WorkingDirectoryOption.class);
-        if (work != null) {
-            m_cache = createTemp( new File ( work.getWorkingDirectory() ) );
-        }else  {
-            m_cache = createTemp(null);
+
+        WorkingDirectoryOption work = getSingleOption( WorkingDirectoryOption.class );
+        if( work != null ) {
+            m_cache = createTemp( new File( work.getWorkingDirectory() ) );
         }
-        
+        else {
+            m_cache = createTemp( null );
+        }
+
         m_store = new TemporaryStore( m_cache, false );
 
         TimeoutOption timeoutOption = getSingleOption( TimeoutOption.class );
-        if (timeoutOption != null) {
+        if( timeoutOption != null ) {
             m_timeout = new RelativeTimeout( timeoutOption.getTimeout() );
-        }else {
+        }
+        else {
             m_timeout = RelativeTimeout.TIMEOUT_DEFAULT;
         }
-        m_clean = getSingleOption(CleanCachesOption.class);
-        
+        m_clean = getSingleOption( CleanCachesOption.class );
+
     }
 
     /**
      * Creates a fresh temp folder under the mentioned working folder.
      * If workingFolder is null, system wide temp location will be used.
-     * 
+     *
      * @param workingDirectory
-     * @return
-     * @throws IOException 
      */
-    private synchronized File createTemp( File workingDirectory ) throws IOException
+    private synchronized File createTemp( File workingDirectory )
+        throws IOException
     {
-        if (workingDirectory == null) {
+        if( workingDirectory == null ) {
             return Files.createTempDir();
-        }else {
+        }
+        else {
             workingDirectory.mkdirs();
             return workingDirectory;
         }
     }
 
     /**
-     * Helper method for single options. First occurence has precedence.
-     * 
-     * @param <T>
-     * @param optionType
-     * @return The first option found of type T or null if none was found.
+     * Helper method for single options. Last occurence has precedence. (support overwrite).
+     *
+     * {@inheritDoc}
+     *
      */
     public <T extends Option> T getSingleOption( final Class<T> optionType )
     {
         m_requestedOptionTypes.add( optionType );
         T[] filter = filter( optionType, m_combinedOptions );
-        
-        if (filter.length > 0) {
-            return filter[0];
-        }else {
+
+        if( filter.length > 0 ) {
+            return filter[ filter.length-1 ];
+        }
+        else {
             return null;
         }
     }
-    
+
     public <T extends Option> T[] getOptions( final Class<T> optionType )
     {
         m_requestedOptionTypes.add( optionType );
@@ -148,7 +179,6 @@ public class DefaultExamSystem implements ExamSystem
     }
 
     /**
-     * 
      * @return the basic directory that Exam should use to look at
      *         user-defaults.
      */
@@ -158,7 +188,6 @@ public class DefaultExamSystem implements ExamSystem
     }
 
     /**
-     * 
      * @return the basic directory that Exam should use for all IO write
      *         activities.
      */
@@ -168,7 +197,6 @@ public class DefaultExamSystem implements ExamSystem
     }
 
     /**
-     * 
      * @return a relative indication of how to deal with timeouts.
      */
     public RelativeTimeout getTimeout()
@@ -177,36 +205,29 @@ public class DefaultExamSystem implements ExamSystem
     }
 
     /**
-     * 
      * Clears up resources taken by system (like temporary files).
-     * 
      */
     public void clear()
     {
-        try
-        {
+        try {
             warnUnusedOptions();
 
-            if (m_clean == null || m_clean.getValue() == Boolean.TRUE ) {
-                for ( ExamSystem sys : m_subsystems )
-                {
+            if( m_clean == null || m_clean.getValue() == Boolean.TRUE ) {
+                for( ExamSystem sys : m_subsystems ) {
                     sys.clear();
                 }
                 Files.deleteRecursively( m_cache.getCanonicalFile() );
-                
+
             }
-        } catch ( IOException e )
-        {
+        } catch( IOException e ) {
             // LOG.info("Clearing " + this.toString() + " failed." ,e);
         }
     }
 
     private void warnUnusedOptions()
     {
-        if ( m_subsystems.isEmpty() )
-        {
-            for ( String skipped : findOptionTypes() )
-            {
+        if( m_subsystems.isEmpty() ) {
+            for( String skipped : findOptionTypes() ) {
                 LOG.warn( "Option " + skipped + " has not been recognized." );
             }
         }
@@ -215,38 +236,34 @@ public class DefaultExamSystem implements ExamSystem
     private Set<String> findOptionTypes()
     {
         Set<String> missing = new HashSet<String>();
-        for ( Option option : m_combinedOptions )
-        {
+        for( Option option : m_combinedOptions ) {
             boolean found = false;
-            for ( Class<?> c : m_requestedOptionTypes )
-            {
-                try
-                {
+            for( Class<?> c : m_requestedOptionTypes ) {
+                try {
                     option.getClass().asSubclass( c );
                     found = true;
                     break;
-                } catch ( Exception e )
-                {
+                } catch( Exception e ) {
 
                 }
             }
-            if ( !found )
-            {
+            if( !found ) {
                 try {
-                    option.getClass().asSubclass(  FrameworkOption.class );
+                    option.getClass().asSubclass( FrameworkOption.class );
                     // false friend
                     continue;
-                }catch(Exception e) {
-                    
+                } catch( Exception e ) {
+
                 }
                 missing.add( option.getClass().getCanonicalName() );
-                
+
             }
         }
         return missing;
     }
 
-    public TestProbeBuilder createProbe( Properties p ) throws IOException
+    public TestProbeBuilder createProbe( Properties p )
+        throws IOException
     {
         return new TestProbeBuilderImpl( p, m_store );
     }
--- core/pax-exam/src/main/java/org/ops4j/pax/exam/ExamSystem.java
+++ core/pax-exam/src/main/java/org/ops4j/pax/exam/ExamSystem.java
@@ -21,55 +21,79 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
-
 /**
  * An instance that drives cross cutting concerns when using Pax Exam.
  * - Provides System Resource Locations,
  * - Cross Cutting default properties,
  * - access to API entry (building probes, getting TestContainerFactory etc.)
- * 
- * @author Toni Menzel ( toni@okidokiteam.com )
  *
+ * @author Toni Menzel ( toni@okidokiteam.com )
  */
 public interface ExamSystem {
-	
-		public  <T extends Option> T[] getOptions( final Class<T> optionType );
-		
-		
-		public ExamSystem fork (Option[] options ) throws IOException;
-		
-		/**
-		 * 
-		 * @return the basic directory that Exam should use to look at user-defaults.
-		 */
-		public File getConfigFolder();
-		
-		/**
-		 * 
-		 * Each call to this method might create a new folder that is being cleared on clear().
-		 * 
-		 * @return the basic directory that Exam should use for all IO write activities.
-		 * 
-		 */
-		public File getTempFolder() ;
-		
-		
-		/**
-		 * 
-		 * @return a relative indication of how to deal with timeouts.
-		 */
-		public RelativeTimeout getTimeout();
-		
-		
-		
-		public TestProbeBuilder createProbe( Properties p ) throws IOException ;
 
+    /**
+     * A shortcut to {@link ExamSystem#getOptions(Class)} when expecting a single value.
+     *
+     * @param optionType type of option to be retrieved.
+     *
+     * @return option matching the parameter type T or null if no option was found. Implementations may rule of their own how to react when multiple values are found (check their javadoc).
+     */
+    public <T extends Option> T getSingleOption( final Class<T> optionType );
 
-		public String createID(String purposeText);
-		
-		/**
-		 * 
-		 * Clears up resources taken by system (like temporary files)
-		 */
-		public void clear();
+    /** 
+     * Getting options of this "system". This is the prime method on accessing options of a giving system.
+     *
+     * @param optionType type of option to be retrieved.
+     * @return options matching the parameter type T. If none was found, this method returns an empty array.
+     */
+    public <T extends Option> T[] getOptions( final Class<T> optionType );
+
+    /**
+     *
+     * @param options options to be used additionally in this fork
+     * @return a forked {@link ExamSystem} instance (new instance). The parent ExamSysten (the one you called fork on) will notice and take care to bring down forked instances on cleanup (for example).
+     *
+     * @throws IOException in case creation of the new {@link ExamSystem} fails. (IO related).
+     */
+    public ExamSystem fork( Option[] options )
+        throws IOException;
+
+    /**
+     * @return the basic directory that Exam should use to look at user-defaults.
+     */
+    public File getConfigFolder();
+
+    /**
+     * Each call to this method might create a new folder that is being cleared on clear().
+     *
+     * @return the basic directory that Exam should use for all IO write activities.
+     */
+    public File getTempFolder();
+
+    /**
+     * @return a relative indication of how to deal with timeouts.
+     */
+    public RelativeTimeout getTimeout();
+
+    /**
+     * New Probe creator using this systems "caches".
+     * @param p initial properties
+     * @return a new {@link TestProbeBuilder} that can be used to create the actual probe.
+     * @throws IOException in case of an IO problem.
+     */
+    public TestProbeBuilder createProbe( Properties p )
+        throws IOException;
+
+    /**
+     * Exam relies on uniquely used identifiers per system. This method gives you one UUID.
+     *
+     * @param purposeText Human readable (short) text that can be used to backtrack the created identifier (for debugging and logging purposes)
+     * @return a new and unqiue identifier.
+     */
+    public String createID( String purposeText );
+
+    /**
+     * Clears up resources taken by system (like temporary files)
+     */
+    public void clear();
 }
