ff413df92fed68d17c1a171fdd0e341476340d8d
Harald Wellmann <harald.wellmann@gmx.de>
2011-08-22 22:16:27 +0200

    [PAXEXAM-250] regression tests for cleanCaches() and keepCaches()

---
+++ it-regression/regression-paxrunner/src/test/java/org/ops4j/pax/exam/regression/paxrunner/cache/CacheTestCleanFalse.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2011 Harald Wellmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.exam.regression.paxrunner.cache;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.ops4j.pax.exam.CoreOptions.cleanCaches;
+import static org.ops4j.pax.exam.CoreOptions.equinox;
+import static org.ops4j.pax.exam.CoreOptions.felix;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.knopflerfish;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.workingDirectory;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.ExamReactorStrategy;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
+import org.osgi.framework.BundleContext;
+
+@RunWith( JUnit4TestRunner.class )
+@ExamReactorStrategy( AllConfinedStagedReactorFactory.class )
+public class CacheTestCleanFalse
+{
+    @Configuration( )
+    public Option[] config()
+    {
+        File workDir = CacheTest.getWorkDir();
+        return options(
+            junitBundles(),
+            workingDirectory( workDir.getAbsolutePath() ),
+            cleanCaches( false ),
+            equinox(),
+            felix(),
+            knopflerfish() );
+    }
+
+    @Test
+    public void countBundles( BundleContext bc )
+    {
+        assertThat(bc.getBundles().length, is(equalTo(8)));
+    }
+}
