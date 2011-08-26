3f5bfd59a85480c4ec02d8e8f67c7ca99f7a5aec
Harald Wellmann <harald.wellmann@gmx.de>
2011-08-26 20:23:04 +0200

    [PAXEXAM-28] Fixed TestNG OSGi manifest in private SNAPSHOT build.
    Use new @Inject feature to make test methods parameterless, which is the
    TestNG default.
    
    Disable tests in Maven builds, as long as we depend on a patched version
    of TestNG.

--- it-regression/regression-native-testng/src/test/java/org/ops4j/pax/exam/testng/test/TestNGExamTest.java
+++ it-regression/regression-native-testng/src/test/java/org/ops4j/pax/exam/testng/test/TestNGExamTest.java
@@ -2,22 +2,57 @@ package org.ops4j.pax.exam.testng.test;
 
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+import static org.testng.Assert.*;
+
+import javax.inject.Inject;
 
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.testng.Configuration;
+import org.osgi.framework.BundleContext;
 import org.testng.annotations.Test;
 
 public class TestNGExamTest
 {
+    @Inject
+    private BundleContext bc;
+    
     @Configuration
     public Option[] config()
     {
-        return options( mavenBundle( "org.testng", "testng", "6.2" ) );
+        return options(
+            
+            /*
+             * The TestNG 6.2 bundle does not resolve due to an incorrect
+             * package version for Guice. The 6.3-SNAPSHOT used here is built
+             * from https://github.com/hwellmann/testng.
+             * A pull request is pending.
+             */
+            
+            mavenBundle( "org.testng", "testng", "6.3-SNAPSHOT" ),
+            
+            /*
+             * The following are optional direct and transitive dependencies
+             * of TestNG which do not seem to be required unless you want
+             * to use command line parameters, Guice injection or YAML suites.
+             */
+            
+            //mavenBundle( "com.beust", "jcommander", "1.12" ),
+            //mavenBundle( "org.beanshell", "com.springsource.bsh", "2.0.0.b4" ),
+            //mavenBundle("org.apache.geronimo.specs", "geronimo-servlet_2.5_spec", "1.2"),
+            //mavenBundle("com.google.inject", "guice", "2.0"),
+            //wrappedBundle(mavenBundle("org.yaml", "snakeyaml", "1.6")),
+
+            mavenBundle( "org.apache.geronimo.specs", "geronimo-atinject_1.0_spec", "1.0" ),
+            mavenBundle( "org.ops4j.pax.exam", "pax-exam-inject", "2.2.1-SNAPSHOT" ),
+            systemProperty( "pax.exam.inject" ).value( "true" ),
+            systemProperty( "osgi.console" ).value( "6666" ) );
     }
 
     @Test
     public void helloTestNG()
     {
+        assertNotNull(bc);
         System.out.println( "Hello TestNG!" );
     }
 
