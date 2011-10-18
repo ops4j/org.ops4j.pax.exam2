cf42e12cf1610e958101ddaa71372c33e068600f
Harald Wellmann <harald.wellmann@gmx.de>
2011-10-18 19:31:44 +0200

    [PAXEXAM-293] Test case for JUnit @Rule

---
+++ it-regression/regression-native-multi/src/test/java/org/ops4j/pax/exam/regression/nat/inject/NameRuleTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2011 Harald Wellmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.exam.regression.nat.inject;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.options;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.ExamReactorStrategy;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.spi.reactors.EagerSingleStagedReactorFactory;
+
+@RunWith(JUnit4TestRunner.class)
+@ExamReactorStrategy(EagerSingleStagedReactorFactory.class)
+public class NameRuleTest {
+
+   @Rule
+   public TestName name = new TestName();
+   
+   @Configuration
+   public Option[] config() {
+       return options(junitBundles());
+   }
+   
+   @Test
+   public void doTest1() throws Exception {
+       assertThat(name.getMethodName(), is(equalTo("doTest1")));
+       assertNotNull("Expected a non null methodName at this point", name.getMethodName());
+   }
+}
