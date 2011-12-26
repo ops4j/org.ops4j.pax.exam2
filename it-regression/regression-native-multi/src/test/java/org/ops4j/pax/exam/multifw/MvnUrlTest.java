01fac316f3665bd7cd07beff44afae42cca65801
Harald Wellmann <harald.wellmann@gmx.de>
2011-12-26 18:43:50 +0100

    use factory SPI to create frameworks, in order to enable or disable framework implementations via Maven profiles

--- it-regression/regression-native-multi/src/test/java/org/ops4j/pax/exam/multifw/MvnUrlTest.java
+++ it-regression/regression-native-multi/src/test/java/org/ops4j/pax/exam/multifw/MvnUrlTest.java
@@ -20,55 +20,59 @@ import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.ServiceLoader;
 
-import org.apache.felix.framework.Felix;
-import org.eclipse.osgi.launch.Equinox;
 import org.junit.Before;
 import org.junit.Test;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.launch.Framework;
+import org.osgi.framework.launch.FrameworkFactory;
 
 /**
- * Not a real Exam Test. We just want to make sure that Equinox and
- * Felix can install bundles directly from mvn: URLs, which does
- * not currently work with JBoss OSGi.
+ * Not a real Exam Test. We just want to make sure that Equinox and Felix can install bundles
+ * directly from mvn: URLs, which does not currently work with JBoss OSGi.
  * 
  * @author Harald Wellmann
- *
+ * 
  */
-public class MvnUrlTest {
+public class MvnUrlTest
+{
 
     private HashMap<String, String> props;
 
     @Before
-    public void setUp() {
+    public void setUp()
+    {
         System.setProperty( "java.protocol.handler.pkgs", "org.ops4j.pax.url" );
         props = new HashMap<String, String>();
     }
-    
-    @Test
-    public void equinoxMvnUrl() throws BundleException, IOException {
-        Framework framework = new Equinox(props);
-        provisionMvnUrl(framework);
-    }
 
     @Test
-    public void felixMvnUrl() throws BundleException, IOException {
-        Framework framework = new Felix(props);
-        provisionMvnUrl(framework);
+    public void provisionMvnUrl() throws BundleException, IOException
+    {
+        ServiceLoader<FrameworkFactory> loader = ServiceLoader.load( FrameworkFactory.class );
+        Iterator<FrameworkFactory> it = loader.iterator();
+        while ( it.hasNext() )
+        {
+            FrameworkFactory frameworkFactory = it.next();
+            Framework framework = frameworkFactory.newFramework( props );
+            provisionToFramework( framework );
+        }
     }
 
-    private void provisionMvnUrl(Framework framework) throws BundleException, IOException {
+    private void provisionToFramework( Framework framework ) throws BundleException, IOException
+    {
         framework.start();
         BundleContext bc = framework.getBundleContext();
-        assertNotNull(bc);
-        
+        assertNotNull( bc );
+
         String url = "mvn:org.ops4j.base/ops4j-base-lang/1.2.3";
-        Bundle bundle = bc.installBundle(url);
-        assertNotNull(bundle);
-        assertEquals("org.ops4j.base.lang", bundle.getSymbolicName());
+        Bundle bundle = bc.installBundle( url );
+        assertNotNull( bundle );
+        assertEquals( "org.ops4j.base.lang", bundle.getSymbolicName() );
 
         bundle.start();
         bundle.uninstall();
