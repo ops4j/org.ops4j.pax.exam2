4df80e8e564b4e610bb7b89e107342f1b1577437
Harald Wellmann <harald.wellmann@gmx.de>
2011-12-27 21:39:14 +0100

    [PAXEXAM-301] adapted to current snapshot of pax-swissbox-framework, use DefaultJavaRunner from ops4j-base-exec

--- containers/pax-exam-container-forked/src/main/java/org/ops4j/pax/exam/forked/ForkedFrameworkFactory.java
+++ containers/pax-exam-container-forked/src/main/java/org/ops4j/pax/exam/forked/ForkedFrameworkFactory.java
@@ -26,50 +26,40 @@ import java.rmi.NotBoundException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
-import org.ops4j.io.Pipe;
+import org.ops4j.base.exec.DefaultJavaRunner;
 import org.ops4j.net.FreePort;
 import org.ops4j.pax.exam.TestContainerException;
 import org.ops4j.pax.swissbox.framework.RemoteFramework;
 import org.ops4j.pax.swissbox.framework.RemoteFrameworkImpl;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.launch.FrameworkFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- * Wraps an OSGi {@link FrameworkFactory} to create and launch a framework in a forked Java
- * virtual machine running in a separate process.
+ * Wraps an OSGi {@link FrameworkFactory} to create and launch a framework in a forked Java virtual
+ * machine running in a separate process.
  * <p>
  * The framework in the forked process can be controlled via a {@link RemoteFramework} interface.
  * 
  * @author Harald Wellmann
- *
+ * 
  */
 public class ForkedFrameworkFactory
 {
-    private static Logger LOG = LoggerFactory.getLogger( ForkedFrameworkFactory.class );
-
     private FrameworkFactory frameworkFactory;
     private File storage;
     private Registry registry;
-    private Process process;
 
     private int port;
 
-    private Pipe errPipe;
-
-    private Pipe outPipe;
-
-    private Pipe inPipe;
+    private DefaultJavaRunner javaRunner;
 
     /**
-     * Creates a ForkedFrameworkFactory wrapping a given OSGi FrameworkFactory and a given
-     * framework storage directory
-     * @param frameworkFactory  OSGi framework factory
+     * Creates a ForkedFrameworkFactory wrapping a given OSGi FrameworkFactory and a given framework
+     * storage directory
+     * 
+     * @param frameworkFactory OSGi framework factory
      * @param storage framework storage directory for the forked framework
      */
     public ForkedFrameworkFactory( FrameworkFactory frameworkFactory, File storage )
@@ -99,11 +89,12 @@ public class ForkedFrameworkFactory
     }
 
     /**
-     * Forks a Java VM process running an OSGi framework and returns a {@link RemoteFramework} handle
-     * to it.
+     * Forks a Java VM process running an OSGi framework and returns a {@link RemoteFramework}
+     * handle to it.
      * <p>
      * TODO add VM properties
-     * @param systemProperties    system properties for the forked Java VM
+     * 
+     * @param systemProperties system properties for the forked Java VM
      * @param frameworkProperties framework properties for the remote framework
      * @return
      * @throws BundleException
@@ -111,83 +102,71 @@ public class ForkedFrameworkFactory
      * @throws InterruptedException
      * @throws NotBoundException
      */
-    public RemoteFramework fork(Map<String, String> systemProperties, Map<String, Object> frameworkProperties) throws BundleException, IOException, InterruptedException, NotBoundException
+    public RemoteFramework fork( Map<String, String> systemProperties,
+            Map<String, Object> frameworkProperties ) throws BundleException, IOException,
+        InterruptedException, NotBoundException
     {
-        
-        FreePort freePort = new FreePort(21000, 21099);
+
+        FreePort freePort = new FreePort( 21000, 21099 );
         port = freePort.getPort();
-        
+
         registry = LocateRegistry.createRegistry( port );
 
-        
-        doFork(systemProperties, frameworkProperties);
+        String[] vmOptions = buildSystemProperties( systemProperties );
+        String[] args = buildFrameworkProperties( frameworkProperties );
+        javaRunner = new DefaultJavaRunner( false );
+        javaRunner.exec( vmOptions, buildClasspath(), RemoteFrameworkImpl.class.getName(),
+            args, getJavaHome(), null );
         return findRemoteFramework();
     }
 
-    private void doFork(Map<String, String> systemProperties, Map<String, Object> frameworkProperties) throws IOException
+    private String[] buildSystemProperties( Map<String, String> systemProperties )
     {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        List<String> args = new ArrayList<String>();
-        args.add( getJavaProgram() );
-        appendSystemProperties(args, systemProperties);
-        
-        // FIXME do not use hard-coded property names and values
-        args.add( "-Dorg.ops4j.pax.exam.rmi.port=" + port );
-        args.add( "-Dorg.ops4j.pax.exam.rmi.name=ExamRemoteFramework"); 
-        
-        args.add( "-cp" );
-        args.add( buildClasspath() );
-        
-        // main class name
-        args.add( RemoteFrameworkImpl.class.getName() );
-        
-        // program arguments: framework properties in key-value pairs
-        args.add( FRAMEWORK_STORAGE );
-        args.add( storage.getAbsolutePath() );
-        appendFrameworkProperties(args, frameworkProperties);
-        
-        processBuilder.command( args );
-        LOG.info("launching remote framework");
-        LOG.info("arguments = {}" , args);
-        process = processBuilder.start();
-        errPipe = new Pipe( process.getErrorStream(), System.err ).start( "Error pipe" );
-        outPipe = new Pipe( process.getInputStream(), System.out ).start( "Out pipe" );
-        inPipe = new Pipe( process.getOutputStream(), System.in ).start( "In pipe" );
-    }
-
-    private void appendSystemProperties( List<String> args, Map<String, String> systemProperties )
-    {
-        for (Map.Entry<String, String> entry : systemProperties.entrySet()) {
-            args.add( String.format("-D%s=%s", entry.getKey(), entry.getValue() ));
+        String[] vmOptions = new String[systemProperties.size() + 2];
+        int i = 0;
+        for ( Map.Entry<String, String> entry : systemProperties.entrySet() )
+        {
+            vmOptions[i++] = String.format( "-D%s=%s", entry.getKey(), entry.getValue() );
         }
+        vmOptions[i++] = "-Dorg.ops4j.pax.exam.rmi.port=" + port;
+        vmOptions[i++] = "-Dorg.ops4j.pax.exam.rmi.name=ExamRemoteFramework";
+        return vmOptions;
     }
 
-    private void appendFrameworkProperties( List<String> args,
-            Map<String, Object> frameworkProperties )
+    private String[] buildFrameworkProperties( Map<String, Object> frameworkProperties )
     {
-        for (Map.Entry<String, Object> entry : frameworkProperties.entrySet()) {
-            args.add( entry.getKey() );
-            args.add( entry.getValue().toString() );
+        String[] args = new String[frameworkProperties.size() + 2];
+        int i = 0;
+        args[i++] = FRAMEWORK_STORAGE;
+        args[i++] = storage.getAbsolutePath();
+        for ( Map.Entry<String, Object> entry : frameworkProperties.entrySet() )
+        {
+
+            args[i++] = entry.getKey();
+            args[i++] = entry.getValue().toString();
         }
+        return args;
     }
 
-
-    private String getJavaProgram()
+    private String getJavaHome()
     {
         String javaHome = System.getenv( "JAVA_HOME" );
-        if (javaHome == null) {
+        if( javaHome == null )
+        {
             javaHome = System.getProperty( "java.home" );
         }
-        File javaBin = new File(javaHome, "bin");
-        File java = new File(javaBin, "java");
-        return java.getAbsolutePath();
+        return javaHome;
     }
 
-    private String buildClasspath()
+    private String[] buildClasspath()
     {
-        String frameworkPath = frameworkFactory.getClass().getProtectionDomain().getCodeSource().getLocation().toString();
-        String launcherPath = RemoteFrameworkImpl.class.getProtectionDomain().getCodeSource().getLocation().toString();
-        return frameworkPath + File.pathSeparator + launcherPath;
+        String frameworkPath =
+            frameworkFactory.getClass().getProtectionDomain().getCodeSource().getLocation()
+                .toString();
+        String launcherPath =
+            RemoteFrameworkImpl.class.getProtectionDomain().getCodeSource().getLocation()
+                .toString();
+        return new String[]{ frameworkPath, launcherPath };
     }
 
     private RemoteFramework findRemoteFramework()
@@ -215,11 +194,12 @@ public class ForkedFrameworkFactory
         }
         catch ( Exception e )
         {
-            throw new TestContainerException( "Cannot get the remote bundle context", e );
+            throw new TestContainerException( "cannot find remote framework in RMI registry", e );
         }
         if( framework == null )
         {
-            throw new TestContainerException( "Cannot get the remote bundle context", reason );
+            throw new TestContainerException( "cannot find remote framework in RMI registry",
+                reason );
         }
         return framework;
 
@@ -227,24 +207,15 @@ public class ForkedFrameworkFactory
 
     /**
      * Waits for the remote framework to shutdown and frees all resources.
-     * <p>
-     * TODO error handling, timeouts etc.
      */
     public void join()
     {
         try
         {
             UnicastRemoteObject.unexportObject( registry, true );
-            process.waitFor();
-            errPipe.stop();
-            outPipe.stop();
-            inPipe.stop();
+            javaRunner.waitForExit();
         }
         catch ( NoSuchObjectException exc )
-        {
-            throw new TestContainerException( exc );
-        }
-        catch ( InterruptedException exc )
         {
             throw new TestContainerException( exc );
         }
