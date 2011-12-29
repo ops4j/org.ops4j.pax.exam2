2907d28555213fb2c33750b069fa18b2ef20997e
Harald Wellmann <harald.wellmann@gmx.de>
2011-12-29 20:20:35 +0100

    [PAXEXAM-318] Run regression tests in multiple containers without copying
    new layout for integration tests based on maven-invoker-pluging, running
    multiple executions of the same test project for different combinations
    of test container and OSGi framework

---
+++ itest/src/it/regression-multi/src/test/java/org/ops4j/pax/exam/regression/multi/InjectTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2011 Harald Wellmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.exam.regression.multi;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.regression.multi.RegressionConfiguration.regressionDefaults;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.ExamReactorStrategy;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.regression.pde.HelloService;
+import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
+
+@RunWith( JUnit4TestRunner.class )
+@ExamReactorStrategy( AllConfinedStagedReactorFactory.class )
+public class InjectTest
+{
+
+    @Inject
+    private HelloService helloService;
+
+    @Configuration( )
+    public Option[] config()
+    {
+        return options(
+            regressionDefaults(),
+            mavenBundle("org.ops4j.pax.exam", "regression-pde-bundle", "2.4.0-SNAPSHOT"),
+            junitBundles() );
+    }
+
+    @Test
+    public void getInjectedService()
+    {
+        assertThat( helloService, is( notNullValue() ) );
+        assertThat( helloService.getMessage(), is( equalTo( "Hello Pax!" ) ) );
+    }
+}
