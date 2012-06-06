a5ffed14013b5cd97ce98c6ecd3aa1d6cef9670a
Harald Wellmann <harald.wellmann@gmx.de>
2012-06-06 21:10:58 +0200

    [PAXEXAM-404] Tomcat Container, first shot

---
+++ containers/pax-exam-container-tomcat/src/test/java/org/ops4j/pax/exam/tomcat/TomcatLauncherTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2012 Harald Wellmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.exam.tomcat;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.apache.catalina.Host;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.util.ContextName;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.ops4j.io.FileUtils;
+import org.ops4j.io.StreamUtils;
+
+import com.google.common.io.Files;
+
+public class TomcatLauncherTest
+{
+
+    private File tempDir;
+
+    @Before
+    public void setUp()
+    {
+        tempDir = Files.createTempDir();
+    }
+
+    @After
+    public void tearDown()
+    {
+        FileUtils.delete( tempDir );
+    }
+
+    @Test
+    public void launchTomcat() throws InterruptedException, IOException, LifecycleException
+    {
+        System.setProperty( "java.protocol.handler.pkgs", "org.ops4j.pax.url" );
+        String tempDir = Files.createTempDir().getAbsolutePath();
+        File baseDir = new File(tempDir, "tomcat");
+        File webappDir = new File(baseDir, "webapps");
+        webappDir.mkdirs();
+        Tomcat tomcat = new Tomcat();
+        tomcat.setBaseDir( baseDir.getPath() );
+        tomcat.setPort(9080);
+        Host host = tomcat.getHost();
+        host.setDeployOnStartup( false );
+        host.setAutoDeploy( false );
+        host.setConfigClass( TomcatContextConfig.class.getName() );
+        TomcatHostConfig hostConfig = new TomcatHostConfig();
+        host.addLifecycleListener( hostConfig );
+        tomcat.start();
+        
+        File warTarget = new File( webappDir, "/wicket-examples.war");
+        InputStream is = new URL("mvn:org.apache.wicket/wicket-examples/1.5.3/war").openStream();
+        FileOutputStream os = new FileOutputStream(warTarget);
+        StreamUtils.copyStream( is, os, true );
+        
+        hostConfig.deployWAR( new ContextName( "/wicket-examples" ), warTarget);
+        hostConfig.unmanageApp( "/wicket-examples" );
+        tomcat.stop();
+    }
+}
---
+++ containers/pax-exam-container-tomcat/src/test/java/org/ops4j/pax/exam/tomcat/TomcatTestContainerTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2012 Harald Wellmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.exam.tomcat;
+
+import static org.ops4j.pax.exam.CoreOptions.war;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.junit.Test;
+import org.ops4j.pax.exam.ExamSystem;
+import org.ops4j.pax.exam.spi.PaxExamRuntime;
+
+public class TomcatTestContainerTest
+{
+
+    @Test
+    public void launchTomcat() throws IOException
+    {
+        ExamSystem system = PaxExamRuntime.createServerSystem( );
+        TomcatTestContainer container = new TomcatTestContainer( system, null );
+        container.start();
+        container.stop();
+    }
+    
+    @Test
+    public void deployWebapp() throws IOException
+    {
+        System.setProperty( "java.protocol.handler.pkgs", "org.ops4j.pax.url" );
+        ExamSystem system = PaxExamRuntime.createServerSystem( war( "mvn:org.apache.wicket/wicket-examples/1.5.3/war" ).name( "wicket-examples" ) );
+        TomcatTestContainer container = new TomcatTestContainer( system, null );
+        container.start();
+        container.deployModules();
+        container.stop();
+    }
+    
+    @Test
+    public void deployWebappFromStream() throws IOException
+    {
+        System.setProperty( "java.protocol.handler.pkgs", "org.ops4j.pax.url" );
+        ExamSystem system = PaxExamRuntime.createServerSystem(  );
+        TomcatTestContainer container = new TomcatTestContainer( system, null );
+        container.start();
+        InputStream is = new URL("mvn:org.apache.wicket/wicket-examples/1.5.3/war").openStream();
+        container.install( is );
+        is.close();
+        container.stop();
+    }
+    
+}
