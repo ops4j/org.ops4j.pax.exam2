c0eaf53ff1fdfd440ceea1618f28ffb8885f07e4
Harald Wellmann <harald.wellmann@gmx.de>
2012-08-19 13:46:17 +0200

    [PAXEXAM-427] regression test for multiple configuration methods per test class

---
+++ itest/src/it/regression-multi/src/test/java/org/ops4j/pax/exam/regression/multi/inject/MultiConfigurationTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2011 Harald Wellmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.exam.regression.multi.inject;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+import static org.ops4j.pax.exam.CoreOptions.url;
+import static org.ops4j.pax.exam.regression.multi.RegressionConfiguration.regressionDefaults;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.regression.pde.HelloService;
+import org.ops4j.pax.exam.util.PathUtils;
+import org.ops4j.pax.swissbox.framework.ServiceLookup;
+import org.osgi.framework.BundleContext;
+
+@RunWith( JUnit4TestRunner.class )
+public class MultiConfigurationTest
+{
+
+    @Inject
+    private BundleContext bundleContext;
+
+
+    @Configuration
+    public Option[] createConfiguration1()
+    {
+        return options(
+            regressionDefaults(),
+            url( "reference:file:" + PathUtils.getBaseDir() +
+                    "/target/regression-pde-bundle.jar" ),
+            systemProperty( "pax.exam.language" ).value( "la" ),
+            junitBundles() );
+    }
+    
+    @Configuration
+    public Option[] createConfiguration2()
+    {
+        return options(
+            regressionDefaults(),
+            systemProperty( "pax.exam.language" ).value( "en" ),
+            url( "reference:file:" + PathUtils.getBaseDir() +
+                    "/target/regression-pde-bundle.jar" ),
+            junitBundles() );
+    }
+    
+    
+
+    @Test
+    public void getServiceFromInjectedBundleContext()
+    {
+        String language = System.getProperty( "pax.exam.language" );
+        String expected = (language.equals( "en" )) ? "Hello Pax!" : "Pax Vobiscum!"; 
+        assertThat( bundleContext, is( notNullValue() ) );
+        Map<String,String> props = new HashMap<String, String>();
+        props.put( "language", language );
+        HelloService service = ServiceLookup.getService( bundleContext, HelloService.class, props );
+        assertThat( service, is( notNullValue() ) );
+        assertThat( service.getMessage(), is(expected));
+    }
+}
