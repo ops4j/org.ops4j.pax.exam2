516b9221b3223939352e2d307ce08102d21b22bf
Harald Wellmann <harald.wellmann@gmx.de>
2012-08-22 18:39:31 +0200

    [PAXEXAM-430] @ExamFactory and @ExamReactorStrategy annotations should be marked as inheritable

--- core/pax-exam/src/main/java/org/ops4j/pax/exam/ExamFactory.java
+++ core/pax-exam/src/main/java/org/ops4j/pax/exam/ExamFactory.java
@@ -18,6 +18,7 @@
 package org.ops4j.pax.exam;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -27,6 +28,7 @@ import java.lang.annotation.Target;
  */
 @Retention( RetentionPolicy.RUNTIME )
 @Target( { ElementType.TYPE } )
+@Inherited
 public @interface ExamFactory
 {
     Class<? extends TestContainerFactory> value();
