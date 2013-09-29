053df6388caa4ad1d2d030da3bfea756c15eea81
Harald Wellmann <harald.wellmann@gmx.de>
2013-09-29 12:40:02 +0200

    [PAXEXAM-575] Regression tests with DeltaSpike Data and DeltaSpike JPA

---
+++ itest/javaee/src/it/regression-javaee-data/src/test/java/org/ops4j/pax/exam/regression/javaee/data/AuthorRepositoryTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2013 Harald Wellmann
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.ops4j.pax.exam.regression.javaee.data;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.sample1.model.Author;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerSuite;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerSuite.class)
+public class AuthorRepositoryTest {
+
+    @Inject
+    private AuthorRepository repo;
+
+    @Test
+    public void byAuthor() {
+        Author author = new Author();
+        author.setFirstName("Heinrich");
+        author.setLastName("Heine");
+        repo.saveAndFlush(author);
+        
+        assertThat(repo.count(), is(3L));
+    }
+}
