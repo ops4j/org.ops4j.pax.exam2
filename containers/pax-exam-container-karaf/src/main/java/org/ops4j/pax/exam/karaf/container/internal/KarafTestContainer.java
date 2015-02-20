1a22ff264cb4003ca5f7e7d87f597a8ec01af86b
Guillaume Nodet <gnodet@gmail.com>
2015-02-20 10:01:39 +0100

    [PAXEXAM-694] Support Karaf libraries in the lib/boot folder

--- containers/pax-exam-container-karaf/src/main/java/org/ops4j/pax/exam/karaf/container/internal/KarafTestContainer.java
+++ containers/pax-exam-container-karaf/src/main/java/org/ops4j/pax/exam/karaf/container/internal/KarafTestContainer.java
@@ -471,6 +471,12 @@ public class KarafTestContainer implements TestContainer {
         for (File jar : jars) {
             cp.add(jar.toString());
         }
+        // do the same for lib/boot
+        File[] bootJars = new File(karafHome + "/lib/boot")
+                .listFiles((FileFilter) new WildcardFileFilter("*.jar"));
+        for (File jar : bootJars) {
+            cp.add(jar.toString());
+        }
         // do the same for lib/ext
         File[] extJars = new File(karafHome + "/lib/ext")
             .listFiles((FileFilter) new WildcardFileFilter("*.jar"));
