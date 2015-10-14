5df0f21a5431be6a15c08aa13d68d38f6ebb9163
Roland Hauser <holyroli82@gmail.com>
2015-10-14 22:43:51 +0200

    Added license

--- containers/pax-exam-container-sisu/src/main/java/org/pax/exam/sisu/SisuTestContainer.java
+++ containers/pax-exam-container-sisu/src/main/java/org/pax/exam/sisu/SisuTestContainer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 Harald Wellmann, 2015 Roland Hauser
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
 package org.pax.exam.sisu;
 
 import static com.google.inject.Guice.createInjector;
@@ -26,6 +42,10 @@ import org.slf4j.LoggerFactory;
 
 import com.google.inject.Injector;
 
+/**
+ * @author Roland Hauser
+ * @since 4.7.0
+ */
 public class SisuTestContainer implements TestContainer {
 
 
