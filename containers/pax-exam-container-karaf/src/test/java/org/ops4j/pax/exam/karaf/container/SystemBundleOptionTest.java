4f57bf09c97121f9535077fe2dbdcb066dc16fd6
Steven Spungin <steven@spungin.tv>
2016-12-12 12:58:26 -0500

    Add SystemBundleOptionTest

---
+++ containers/pax-exam-container-karaf/src/test/java/org/ops4j/pax/exam/karaf/container/SystemBundleOptionTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2011 Harald Wellmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.exam.karaf.container;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.karaf.options.KarafDistributionOption;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
+import org.ops4j.pax.swissbox.tracker.ServiceLookup;
+import org.osgi.framework.BundleContext;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
+public class SystemBundleOptionTest extends KarafTestContainerITest {
+
+    @Inject
+    BundleContext bc;
+
+    @Configuration
+    public Option[] config() {
+        ArrayList<Option> opts = new ArrayList<>(Arrays.asList(super.config()));
+        opts.add(KarafDistributionOption.systemBundle("org.ops4j.pax.exam", "regression-pde-bundle",
+            "2.3.0"));
+        return opts.toArray(new Option[0]);
+    }
+
+    @Test
+    public void getService() {
+        Object service = ServiceLookup.getService(bc,
+            "org.ops4j.pax.exam.regression.pde.HelloService");
+        assertThat(service, is(notNullValue()));
+    }
+}
