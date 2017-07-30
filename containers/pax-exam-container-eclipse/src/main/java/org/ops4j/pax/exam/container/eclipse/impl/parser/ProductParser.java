8303c0a18f6a3847d1a5c3bf1bf473f46fa161ed
Laeubi <Laeubi@Lenovo-X1Carbon>
2017-07-30 14:13:46 +0200

    PAXEXAM-832 - some bugfixes and speed improvements

--- containers/pax-exam-container-eclipse/src/main/java/org/ops4j/pax/exam/container/eclipse/impl/parser/ProductParser.java
+++ containers/pax-exam-container-eclipse/src/main/java/org/ops4j/pax/exam/container/eclipse/impl/parser/ProductParser.java
@@ -19,12 +19,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.xml.xpath.XPathExpressionException;
 
+import org.ops4j.pax.exam.container.eclipse.EclipseVersionedArtifact;
 import org.ops4j.pax.exam.container.eclipse.impl.ArtifactInfo;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -37,25 +38,37 @@ import org.w3c.dom.Node;
  */
 public class ProductParser extends AbstractParser {
 
-    private List<ArtifactInfo<PluginConfiguration>> plugins = new ArrayList<>();
+    private final List<EclipseVersionedArtifact> plugins = new ArrayList<>();
+    private final List<EclipseVersionedArtifact> features = new ArrayList<>();
+    private final Map<String, PluginConfiguration> configuration = new LinkedHashMap<>();
+    private String application;
+    private String productID;
+    private boolean useFeatures;
 
     public ProductParser(InputStream stream) throws IOException {
         try {
             Element document = parse(stream);
+            application = getAttribute(document, "application", false);
+            productID = getAttribute(document, "id", false);
+            useFeatures = Boolean.parseBoolean(getAttribute(document, "useFeatures", false));
             // fetch configs
-            Map<String, PluginConfiguration> configs = new HashMap<>();
             for (Node node : evaluate(document, "/product/configurations/plugin")) {
                 String id = getAttribute(node, "id", true);
                 boolean autoStart = Boolean.parseBoolean(getAttribute(node, "autoStart", false));
                 int startLevel = Integer.parseInt(getAttribute(node, "startLevel", true));
-                configs.put(id, new PluginConfiguration(autoStart, startLevel));
+                configuration.put(id, new PluginConfiguration(autoStart, startLevel));
             }
             // fetch plugins
             for (Node node : evaluate(document, "/product/plugins/plugin")) {
                 String id = getAttribute(node, "id", true);
                 String version = getAttribute(node, "version", false);
-                plugins.add(new ArtifactInfo<PluginConfiguration>(id, stringToVersion(version),
-                    configs.get(id)));
+                plugins.add(new ArtifactInfo<Void>(id, stringToVersion(version), null));
+            }
+            // fetch features
+            for (Node node : evaluate(document, "/product/features/feature")) {
+                String id = getAttribute(node, "id", true);
+                String version = getAttribute(node, "version", false);
+                features.add(new ArtifactInfo<Void>(id, stringToVersion(version), null));
             }
         }
         catch (XPathExpressionException e) {
@@ -63,8 +76,28 @@ public class ProductParser extends AbstractParser {
         }
     }
 
-    public List<ArtifactInfo<PluginConfiguration>> getPlugins() {
+    public String getApplication() {
+        return application;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public boolean useFeatures() {
+        return useFeatures;
+    }
+
+    public List<EclipseVersionedArtifact> getPlugins() {
         return Collections.unmodifiableList(plugins);
+    }
+
+    public Map<String, PluginConfiguration> getConfiguration() {
+        return Collections.unmodifiableMap(configuration);
+    }
+
+    public List<EclipseVersionedArtifact> getFeatures() {
+        return Collections.unmodifiableList(features);
     }
 
     public static final class PluginConfiguration {
