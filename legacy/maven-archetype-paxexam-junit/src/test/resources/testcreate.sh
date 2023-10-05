7cb2a418133d5ffb02838984a1982a445ba08744
Oliver Lietz <info@oliverlietz.de>
2023-10-05 23:05:52 +0200

    #1094 Drop legacy modules

--- legacy/maven-archetype-paxexam-junit/src/test/resources/testcreate.sh
+++
@@ -1,1 +0,0 @@
-mvn archetype:create -DarchetypeGroupId=org.ops4j.pax.exam -DarchetypeArtifactId=maven-archetype-paxexam-junit -DarchetypeVersion=1.2.0-SNAPSHOT -DgroupId=com.company -DartifactId=company-osgitests
